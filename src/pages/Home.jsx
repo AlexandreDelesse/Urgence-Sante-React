10b43ba7d83640752d386f1f9113687f17619afd
Alexandre Delesse <alexandre.delesse@isen.yncrea.fr>
2022-06-16 22:56:35 +0200

    add version on first page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Container } from "react-bootstrap";
 import UserContext from "../contexts/User.context";
+import packageJson from "../../package.json";
 
 export default function Home() {
   const { user } = useContext(UserContext);
@@ -8,6 +9,9 @@ export default function Home() {
     <Container>
       {user && <h1 className="text-center mt-5">Bonjour {user.name} !</h1>}
       <h2 className="text-center mt-5">Bienvenue sur la page principale</h2>
+      <p className="text-center mt3">
+        version de l'app : {packageJson.version}
+      </p>
     </Container>
   );
 }
