c37151d4269e19541cab846cd74b17a28568a8e5
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2023-08-19 00:43:04 +0200

    add space above loader

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -3,9 +3,9 @@ import { Spinner } from 'react-bootstrap'
 
 export default function Loader({ loadingMessage }) {
   return (
-    <div className="d-flex w-100 flex-column justify-content-center align-items-center">
+    <div className="mt-3 d-flex w-100 flex-column justify-content-center align-items-center">
       <Spinner animation="border" variant="warning" />
-      {loadingMessage && <p className='mt-3'>{loadingMessage}</p>}
+      {loadingMessage && <p className="mt-3">{loadingMessage}</p>}
     </div>
   )
 }
