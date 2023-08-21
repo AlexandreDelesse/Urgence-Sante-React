007e0b3cd7d7a54230808525d65bb9673a9948f1
Alexandre Delesse <alexandre.delesse@isen.yncrea.fr>
2023-08-21 10:43:51 +0200

    filtrer les missions terminées + bug fixes sur retour d'api missions

--- src/components/mission/MissionList.jsx
+++ src/components/mission/MissionList.jsx
@@ -1,94 +1,114 @@
-import React, { useEffect } from 'react'
-import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
-import { useQuery, useQueryClient } from 'react-query'
-import { Navigate, useNavigate } from 'react-router-dom'
-import { acceptMission, getMissions } from '../../services/mission.service'
-import AsyncDataComponent from '../shared/AsyncDataComponent'
-import DateFormatter from '../shared/DateFormatter'
-import TransportType from '../shared/TransportType'
-import { BsCheck2Square } from 'react-icons/bs'
-import './mission.css'
-import { transportModeEnum } from '../../data/enum.data'
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  Form,
+  ListGroup,
+  ListGroupItem,
+  ToggleButton,
+} from "react-bootstrap";
+import { useQuery, useQueryClient } from "react-query";
+import { Navigate, useNavigate } from "react-router-dom";
+import { acceptMission, getMissions } from "../../services/mission.service";
+import AsyncDataComponent from "../shared/AsyncDataComponent";
+import DateFormatter from "../shared/DateFormatter";
+import TransportType from "../shared/TransportType";
+import { BsCheck2Square } from "react-icons/bs";
+import "./mission.css";
+import { transportModeEnum } from "../../data/enum.data";
 
 export default function MissionList() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [showTerminated, setShowTerminated] = useState(false);
 
-  const asyncMissions = useQuery('missions', getMissions)
-  const queryClient = useQueryClient()
+  const asyncMissions = useQuery("missions", getMissions);
+  const queryClient = useQueryClient();
 
   const onMissionClick = (missionIndex) => {
-    navigate(`/jobdetail/${missionIndex}`)
-  }
+    navigate(`/jobdetail/${missionIndex}`);
+  };
 
   const onButtonClick = async (e, jobId) => {
-    e.stopPropagation()
-    await acceptMission(jobId)
-    queryClient.invalidateQueries('missions')
-  }
+    e.stopPropagation();
+    await acceptMission(jobId);
+    queryClient.invalidateQueries("missions");
+  };
 
-  useEffect(() => {
-    console.log(asyncMissions)
-  }, [asyncMissions])
+  const toggleShowTerminated = () => {
+    setShowTerminated((old) => !old);
+  };
 
   return (
-    <AsyncDataComponent
-      data={asyncMissions}
-      onLoadingMessage="Chargement des missions.."
-      onSuccess={({ data }) => (
-        <div className="mt-3">
-          {data.length === 0 ? (
-            <div>Pas de missions</div>
-          ) : (
-            <div>
-              <ListGroup variant="flush">
-                {data.map((el) => (
-                  <ListGroupItem
-                    key={el.index}
-                    onClick={() => onMissionClick(el.jobId)}
-                    className="d-flex justify-content-between px-0"
-                  >
-                    <div className="job-item w-100">
-                      <span
-                        className={el.isAck ? 'bg-success' : 'bg-warning'}
-                      />
-                      <div className="d-flex flex-column ms-2">
-                        <div className="d-flex justify-content-between">
-                          <div>
-                            <div>{el.schedule}</div>
+    <>
+      <Form.Check
+        className="my-2"
+        type="switch"
+        label="Afficher les missions terminées"
+        checked={showTerminated}
+        onChange={toggleShowTerminated}
+      />
+      <AsyncDataComponent
+        data={asyncMissions}
+        onLoadingMessage="Chargement des missions.."
+        onSuccess={({ data }) => (
+          <div>
+            {data.length === 0 ? (
+              <div>Pas de missions</div>
+            ) : (
+              <div>
+                <ListGroup variant="flush">
+                  {data
+                    .filter((el) => showTerminated || !el.isTerminated)
+                    .sort((a, b) => b.index - a.index)
+                    .map((el, index) => (
+                      <ListGroupItem
+                        key={index}
+                        onClick={() => onMissionClick(el.jobId)}
+                        className="d-flex justify-content-between px-0"
+                      >
+                        <div className="job-item w-100">
+                          <span
+                            className={el.isAck ? "bg-success" : "bg-warning"}
+                          />
+                          <div className="d-flex flex-column ms-2">
+                            <div className="d-flex justify-content-between">
+                              <div>
+                                <div>{el.schedule}</div>
+                              </div>
+                              {el.isAck || (
+                                <Button
+                                  size="sm"
+                                  onClick={(e) => onButtonClick(e, el.jobId)}
+                                  variant="success"
+                                >
+                                  Accepter <BsCheck2Square size={16} />
+                                </Button>
+                              )}
+                            </div>
+                            <span>{transportModeEnum[el.transportMode]}</span>
+                            <div className="fw-bold">{el.patient}</div>
+                            <div>Rdv : {el.appointment || "Pas de rdv"}</div>
+                            <div>
+                              <span className="fw-bold">Départ :</span>
+                              {el.departure}
+                            </div>
+                            <div>
+                              <span className="fw-bold">Arrivée :</span>
+                              {el.arrival}
+                            </div>
+                            <div>
+                              <TransportType transportType={el.transportType} />
+                            </div>
                           </div>
-                          {el.isAck || (
-                            <Button
-                              size="sm"
-                              onClick={(e) => onButtonClick(e, el.jobId)}
-                              variant="success"
-                            >
-                              Accepter <BsCheck2Square size={16} />
-                            </Button>
-                          )}
                         </div>
-                        <span>{transportModeEnum[el.transportMode]}</span>
-                        <div className="fw-bold">{el.patient}</div>
-                        <div>Rdv : {el.appointment || 'Pas de rdv'}</div>
-                        <div>
-                          <span className="fw-bold">Départ :</span>
-                          {el.departure}
-                        </div>
-                        <div>
-                          <span className="fw-bold">Arrivée :</span>
-                          {el.arrival}
-                        </div>
-                        <div>
-                          <TransportType transportType={el.transportType} />
-                        </div>
-                      </div>
-                    </div>
-                  </ListGroupItem>
-                ))}
-              </ListGroup>
-            </div>
-          )}
-        </div>
-      )}
-    ></AsyncDataComponent>
-  )
+                      </ListGroupItem>
+                    ))}
+                </ListGroup>
+              </div>
+            )}
+          </div>
+        )}
+      ></AsyncDataComponent>
+    </>
+  );
 }
--- src/services/mission.service.js
+++ src/services/mission.service.js
@@ -1,40 +1,43 @@
-import { mission, missions } from '../data/mission.data'
-import { api } from './api.config'
-import { getToken } from './user.service'
+import { mission, missions } from "../data/mission.data";
+import { api } from "./api.config";
+import { getToken } from "./user.service";
 
 const getMissions = async () => {
   try {
-    const token = getToken()
-    if (!token) throw new Error('Pas de token. Veuillez vous authentifier')
-    const { data: missions } = await api.get('/JobList', {
+    const token = getToken();
+    if (!token) throw new Error("Pas de token. Veuillez vous authentifier");
+    const { data: missions } = await api.get("/JobList", {
       params: { gCrewToken: token },
-    })
-    return missions
+    });
+    return typeof missions === "object" ? missions : [];
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const getMissionById = async (jobId) => {
   try {
-    const { data: jobDetail } = await api.get('/jobDetail', {
+    const { data: jobDetail } = await api.get("/jobDetail", {
       params: { gJobId: jobId },
       // headers: {
       //   Authorization: getToken(),
       // },
-    })
-    return jobDetail
+    });
+    return jobDetail;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const acceptMission = async (missionId) => {
   try {
-    return await api.patch('/joblist', { jobId: missionId, acknowledged: true })
+    return await api.patch("/joblist", {
+      jobId: missionId,
+      acknowledged: true,
+    });
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
-export { getMissions, getMissionById, acceptMission }
+export { getMissions, getMissionById, acceptMission };
