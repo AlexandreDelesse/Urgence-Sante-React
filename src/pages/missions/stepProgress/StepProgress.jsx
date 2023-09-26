5da044c48bab08ddaa75e4a5a7d740339f4041b3
Alexandre Delesse <alexandre.delesse@isen.yncrea.fr>
2023-09-26 15:10:19 +0200

    step-progress-for-mission

--- src/pages/missions/stepProgress/StepProgress.jsx
+++ src/pages/missions/stepProgress/StepProgress.jsx
@@ -1,24 +1,57 @@
 import React, { useState } from "react";
 import "./stepProgress.css";
-import { Step, StepLabel, Button, Stepper, Typography } from "@mui/material";
+import {
+  Step,
+  StepLabel,
+  Button,
+  Stepper,
+  Typography,
+  StepButton,
+  Modal,
+  Box,
+} from "@mui/material";
+import { FormControl } from "react-bootstrap";
 
 export default function StepProgress() {
-  const [activeStep, setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(-1);
   const [completed, setCompleted] = useState({});
   const [steps, setSteps] = useState([
     { label: "En route", time: null },
     { label: "En charge", time: null },
     { label: "Dispo", time: null },
   ]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editingStep, setEditingStep] = useState(null);
+  const [editingStepValue, setEditingStepValue] = useState(null);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 300,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const goNextStep = () => {
     handleComplete();
     if (activeStep < steps.length) setActiveStep((old) => old + 1);
   };
 
+  const updateStep = (stepindex, value) => {
+    setSteps((old) =>
+      old.map((step, index) =>
+        stepindex === index ? { ...step, time: value } : step
+      )
+    );
+  };
+
   const handleComplete = () => {
     const newSteps = steps.map((step, index) =>
-      index === activeStep
+      index === activeStep + 1
         ? {
             ...step,
             time: new Date().toLocaleTimeString("fr-FR", {
@@ -30,26 +63,74 @@ export default function StepProgress() {
     );
     setSteps(newSteps);
     const newCompleted = completed;
-    newCompleted[activeStep] = true;
+    newCompleted[activeStep + 1] = true;
     setCompleted(newCompleted);
+  };
+
+  const toggleModalopen = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const handleOnEditTime = (index) => {
+    setEditingStep(index);
+    setEditingStepValue(steps[index].time);
+    toggleModalopen();
+  };
+
+  const updateEditingStep = (e) => setEditingStepValue(e.target.value);
+
+  const onValidateUpdateStep = () => {
+    updateStep(editingStep, editingStepValue);
+    toggleModalopen();
   };
 
   return (
     <div>
-      <Button onClick={goNextStep}>
-        {steps[activeStep] ? steps[activeStep].label : ""}
-      </Button>
+      <div className="d-flex justify-content-end">
+        <Button className="my-2" onClick={goNextStep}>
+          {steps[activeStep + 1] ? steps[activeStep + 1].label : ""}
+        </Button>
+      </div>
       <Stepper activeStep={activeStep} alternativeLabel>
         {steps.map((step, index) => (
           <Step key={step.label} completed={completed[index]}>
-            <StepLabel>
+            <StepButton onClick={() => handleOnEditTime(index)}>
               {step.label}
               <br />
               <Typography variant="caption">{step.time}</Typography>
-            </StepLabel>
+            </StepButton>
           </Step>
         ))}
       </Stepper>
+
+      {editingStep >= 0 && (
+        <Modal open={modalOpen} onClose={toggleModalopen}>
+          <Box sx={style}>
+            <Typography variant="h5">Mettre à jour l'heure</Typography>
+            <FormControl
+              onChange={updateEditingStep}
+              value={editingStepValue}
+              type="time"
+            />
+            <div className="d-flex gap-2 my-3">
+              <Button
+                onClick={onValidateUpdateStep}
+                variant="contained"
+                color="success"
+              >
+                Valider
+              </Button>
+              <Button
+                onClick={toggleModalopen}
+                color="secondary"
+                variant="outlined"
+              >
+                Annuler
+              </Button>
+            </div>
+          </Box>
+        </Modal>
+      )}
     </div>
   );
 }
