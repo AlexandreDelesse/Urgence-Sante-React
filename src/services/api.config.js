60a46f8ac53b6e75398df0e6bfe8c860b02807d9
Alexandre Delesse <alexandre.delesse@isen.yncrea.fr>
2023-10-05 15:22:32 +0200

    setup for build prod

--- src/services/api.config.js
+++ src/services/api.config.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://intranet.urgencesante.fr:8090/api',
+  baseURL: process.env.API_URL || "http://intranet.urgencesante.fr:8080/api",
   timeout: 1000,
-})
+});
 
-export { api }
+export { api };
