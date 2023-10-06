4394183b256d0cda6e5ad32be47466b3750b86e6
Alexandre Delesse <alexandre.delesse@isen.yncrea.fr>
2023-10-06 15:53:21 +0200

    remove-comment

--- src/pages/missions/missionList/MissionList.jsx
+++ src/pages/missions/missionList/MissionList.jsx
@@ -69,53 +69,6 @@ const ListeMission = ({
   if (data.length === 0)
     return <div className="text-center mt-3">Pas de missions en cours</div>;
 
-  // return (
-  //   <ListGroup variant="flush" className="scroll-component mission-list">
-  //     {data.map((el, index) => (
-  //       <ListGroupItem
-  //         key={index}
-  //         onClick={() => onMissionClick(el.jobId)}
-  //         className="d-flex justify-content-between px-0"
-  //       >
-  //         <div className="job-item w-100 ">
-  //           <span className={el.isAck ? "bg-success" : "bg-warning"} />
-  //           <div className="d-flex flex-column ms-2">
-  //             <div className="d-flex justify-content-between">
-  //               <div>
-  //                 <div>{el.schedule}</div>
-  //               </div>
-  //               {el.isAck || (
-  //                 <IconButton
-  //                   size="sm"
-  //                   onClick={(e) => onButtonClick(e, el.jobId)}
-  //                   variant="success"
-  //                   icon={<BsCheck2Square size={16} />}
-  //                   isLoading={loadingButton === el.jobId}
-  //                   spinnerVariant="light"
-  //                   label="Bien recus"
-  //                 />
-  //               )}
-  //             </div>
-  //             <span>{transportModeEnum[el.transportMode]}</span>
-  //             <div className="fw-bold">{el.patient}</div>
-  //             <div>Rdv : {el.appointment || "Pas de rdv"}</div>
-  //             <div>
-  //               <span className="fw-bold">Départ :</span>
-  //               {el.departure}
-  //             </div>
-  //             <div>
-  //               <span className="fw-bold">Arrivée :</span>
-  //               {el.arrival}
-  //             </div>
-  //             <div>
-  //               <TransportType transportType={el.transportType} />
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </ListGroupItem>
-  //     ))}
-  //   </ListGroup>
-  // );
   return (
     <div className="scroll-component mission-list">
       {data.map((shortJob, index) => (
